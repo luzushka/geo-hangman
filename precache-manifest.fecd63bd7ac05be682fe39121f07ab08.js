dbb2ee85e32915c7c1790d4c04a303425c55a815
luzushka <yonatan.lozinsky@gmail.com>
2018-11-03 01:40:11 +0200

    Updates

---
+++ precache-manifest.fecd63bd7ac05be682fe39121f07ab08.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "10f9b3f1ccf232a562b10b1060a2b640",
+    "url": "/geo-hangman/static/media/2.10f9b3f1.png"
+  },
+  {
+    "revision": "d3e5a03c87b2f702b7d4",
+    "url": "/geo-hangman/static/css/main.a37eef66.chunk.css"
+  },
+  {
+    "revision": "f8443bdc4a9407e960ec",
+    "url": "/geo-hangman/static/js/1.f8443bdc.chunk.js"
+  },
+  {
+    "revision": "dab5de654139b278aa39",
+    "url": "/geo-hangman/static/js/runtime~main.dab5de65.js"
+  },
+  {
+    "revision": "1e11c061648a3537ee78d040f0aa73f5",
+    "url": "/geo-hangman/static/media/0.1e11c061.png"
+  },
+  {
+    "revision": "66f238616f21ebc64e3f1e1a3e84388d",
+    "url": "/geo-hangman/static/media/1.66f23861.png"
+  },
+  {
+    "revision": "d3e5a03c87b2f702b7d4",
+    "url": "/geo-hangman/static/js/main.d3e5a03c.chunk.js"
+  },
+  {
+    "revision": "a3810e970250c8651cb446879de1873f",
+    "url": "/geo-hangman/static/media/3.a3810e97.png"
+  },
+  {
+    "revision": "2825f89859c7f83e0529fe9ca4119178",
+    "url": "/geo-hangman/static/media/4.2825f898.png"
+  },
+  {
+    "revision": "8e4279042c6620612d8abf6947f2fe09",
+    "url": "/geo-hangman/static/media/background.8e427904.png"
+  },
+  {
+    "revision": "7de4544cc3e616ff945907bf07e2f271",
+    "url": "/geo-hangman/static/media/KGSecondChancesSketch.7de4544c.ttf"
+  },
+  {
+    "revision": "9f23b8ca3246b533c2f724a67bf61459",
+    "url": "/geo-hangman/static/media/DJB Chalk It Up.9f23b8ca.ttf"
+  },
+  {
+    "revision": "1c2517939ba24b41b67bfdceb5ed0c58",
+    "url": "/geo-hangman/index.html"
+  }
+];
